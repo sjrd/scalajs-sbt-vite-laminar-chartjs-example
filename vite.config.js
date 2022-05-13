bf259e24e341ab16795f34f1411630846035fbbd
Sébastien Doeraene <sjrdoeraene@gmail.com>
2022-05-13 10:11:14 +0200

    Make the Vite config work on Windows.

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,22 @@ function isDev() {
 }
 
 function printSbtTask(task) {
-  const result = spawnSync("sbt", ["--error", "--batch", `print ${task}`], {
+  const args = ["--error", "--batch", `print ${task}`];
+  const options = {
     stdio: [
       "pipe", // StdIn.
       "pipe", // StdOut.
       "inherit", // StdErr.
     ],
-  });
+  };
+  const result = process.platform === 'win32'
+    ? spawnSync("sbt.bat", args.map(x => `"${x}"`), {shell: true, ...options})
+    : spawnSync("sbt", args, options);
 
+  if (result.error)
+    throw result.error;
+  if (result.status !== 0)
+    throw new Error(`sbt process failed with exit code ${result.status}`);
   return result.stdout.toString('utf8').trim();
 }
 

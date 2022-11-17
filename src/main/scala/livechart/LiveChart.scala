393c52ff885a4c7ea93d91463a5560a7d67d875e
Sébastien Doeraene <sjrdoeraene@gmail.com>
2022-11-17 13:27:20 +0100

    Data table.

--- src/main/scala/livechart/LiveChart.scala
+++ src/main/scala/livechart/LiveChart.scala
@@ -7,10 +7,6 @@ import com.raquo.laminar.api.L.{*, given}
 
 import org.scalajs.dom
 
-// import javaScriptLogo from "/javascript.svg"
-@js.native @JSImport("/javascript.svg", JSImport.Default)
-val javascriptLogo: String = js.native
-
 @main
 def LiveChart(): Unit =
   renderOnDomContentLoaded(
@@ -41,29 +37,28 @@ object Main:
 
   def appElement(): Element =
     div(
-      a(href := "https://vitejs.dev", target := "_blank",
-        img(src := "/vite.svg", className := "logo", alt := "Vite logo"),
-      ),
-      a(href := "https://developer.mozilla.org/en-US/docs/Web/JavaScript", target := "_blank",
-        img(src := javascriptLogo, className := "logo vanilla", alt := "JavaScript logo"),
-      ),
-      h1("Hello Laminar!"),
-      div(className := "card",
-        counterButton(),
-      ),
-      p(className := "read-the-docs",
-        "Click on the Vite logo to learn more",
-      ),
+      h1("Live Chart"),
+      renderDataTable(),
     )
   end appElement
 
-  def counterButton(): Element =
-    val counter = Var(0)
-    button(
-      tpe := "button",
-      "count is ",
-      child.text <-- counter,
-      onClick --> { event => counter.update(c => c + 1) },
+  def renderDataTable(): Element =
+    table(
+      thead(tr(th("Label"), th("Value"), th("Action"))),
+      tbody(
+        children <-- dataSignal.map(data => data.map { item =>
+          renderDataItem(item.id, item)
+        }),
+      ),
+      tfoot(tr(td(button("+", onClick --> (_ => addDataItem(DataItem())))))),
     )
-  end counterButton
+  end renderDataTable
+
+  def renderDataItem(id: DataItemID, item: DataItem): Element =
+    tr(
+      td(item.label),
+      td(item.value),
+      td(button("🗑️", onClick --> (_ => removeDataItem(id)))),
+    )
+  end renderDataItem
 end Main

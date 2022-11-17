3178dbd16607ddb5b187c8ce3c2a1c8b2297faaf
Sébastien Doeraene <sjrdoeraene@gmail.com>
2022-11-17 17:06:14 +0100

    Render the data chart.

--- src/main/scala/livechart/LiveChart.scala
+++ src/main/scala/livechart/LiveChart.scala
@@ -39,6 +39,7 @@ object Main:
     div(
       h1("Live Chart"),
       renderDataTable(),
+      renderDataChart(),
     )
   end appElement
 
@@ -61,4 +62,65 @@ object Main:
       td(button("🗑️", onClick --> (_ => removeDataItem(id)))),
     )
   end renderDataItem
+
+  /** Chart.js configuration for a bar chart with one dataset. */
+  val chartConfig =
+    import typings.chartJs.mod.*
+    new ChartConfiguration {
+      `type` = ChartType.bar
+      data = new ChartData {
+        datasets = js.Array(new ChartDataSets {
+          label = "Value"
+          borderWidth = 1
+        })
+      }
+      options = new ChartOptions {
+        scales = new ChartScales {
+          yAxes = js.Array(new CommonAxe {
+            ticks = new TickOptions {
+              beginAtZero = true
+            }
+          })
+        }
+      }
+    }
+  end chartConfig
+
+  def renderDataChart(): Element =
+    import scala.scalajs.js.JSConverters.*
+    import typings.chartJs.mod.*
+
+    var optChart: Option[Chart] = None
+
+    canvas(
+      // Regular properties of the canvas
+      width := "100%",
+      height := "200px",
+
+      // onMountUnmount callback to bridge the Laminar world and the Chart.js world
+      onMountUnmountCallback(
+        // on mount, create the `Chart` instance and store it in optChart
+        mount = { nodeCtx =>
+          val domCanvas: dom.HTMLCanvasElement = nodeCtx.thisNode.ref
+          val chart = Chart.apply.newInstance2(domCanvas, chartConfig)
+          optChart = Some(chart)
+        },
+        // on unmount, destroy the `Chart` instance
+        unmount = { thisNode =>
+          for (chart <- optChart)
+            chart.destroy()
+          optChart = None
+        }
+      ),
+
+      // Bridge the FRP world of dataSignal to the imperative world of the `chart.data`
+      dataSignal --> { data =>
+        for (chart <- optChart) {
+          chart.data.labels = data.map(_.label).toJSArray
+          chart.data.datasets.get(0).data = data.map(_.value).toJSArray
+          chart.update()
+        }
+      },
+    )
+  end renderDataChart
 end Main

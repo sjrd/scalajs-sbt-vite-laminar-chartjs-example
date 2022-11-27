31ead23cae6c57997532f9040a8fd968487859e3
Sébastien Doeraene <sjrdoeraene@gmail.com>
2022-11-27 12:04:56 +0100

    Edit items.

--- src/main/scala/livechart/LiveChart.scala
+++ src/main/scala/livechart/LiveChart.scala
@@ -43,6 +43,7 @@ object Main:
     div(
       h1("Live Chart"),
       renderDataTable(),
+      renderDataList(),
     )
   end appElement
 
@@ -65,13 +66,90 @@ object Main:
 
   def renderDataItem(id: DataItemID, itemSignal: Signal[DataItem]): Element =
     tr(
-      td(child.text <-- itemSignal.map(_.label)),
-      td(child.text <-- itemSignal.map(_.price)),
-      td(child.text <-- itemSignal.map(_.count)),
+      td(
+        inputForString(
+          itemSignal.map(_.label),
+          makeDataItemUpdater(id, { (item, newLabel) =>
+            item.copy(label = newLabel)
+          })
+        )
+      ),
+      td(
+        inputForDouble(
+          itemSignal.map(_.price),
+          makeDataItemUpdater(id, { (item, newPrice) =>
+            item.copy(price = newPrice)
+          })
+        )
+      ),
+      td(
+        inputForInt(
+          itemSignal.map(_.count),
+          makeDataItemUpdater(id, { (item, newCount) =>
+            item.copy(count = newCount)
+          })
+        )
+      ),
       td(
         child.text <-- itemSignal.map(item => "%.2f".format(item.fullPrice))
       ),
       td(button("🗑️", onClick --> (_ => removeDataItem(id)))),
     )
   end renderDataItem
+
+  def makeDataItemUpdater[A](id: DataItemID,
+      f: (DataItem, A) => DataItem): Observer[A] =
+    dataVar.updater { (data, newValue) =>
+      data.map { item =>
+        if item.id == id then f(item, newValue) else item
+      }
+    }
+  end makeDataItemUpdater
+
+  def inputForString(valueSignal: Signal[String],
+      valueUpdater: Observer[String]): Input =
+    input(
+      typ := "text",
+      value <-- valueSignal,
+      onInput.mapToValue --> valueUpdater,
+    )
+  end inputForString
+
+  def inputForDouble(valueSignal: Signal[Double],
+      valueUpdater: Observer[Double]): Input =
+    val strValue = Var[String]("")
+    input(
+      typ := "text",
+      value <-- strValue.signal,
+      onInput.mapToValue --> strValue,
+      valueSignal --> strValue.updater[Double] { (prevStr, newValue) =>
+        if prevStr.toDoubleOption.contains(newValue) then prevStr
+        else newValue.toString
+      },
+      strValue.signal --> { valueStr =>
+        valueStr.toDoubleOption.foreach(valueUpdater.onNext)
+      },
+    )
+  end inputForDouble
+
+  def inputForInt(valueSignal: Signal[Int],
+      valueUpdater: Observer[Int]): Input =
+    input(
+      typ := "text",
+      controlled(
+        value <-- valueSignal.map(_.toString),
+        onInput.mapToValue.map(_.toIntOption).collect {
+          case Some(newCount) => newCount
+        } --> valueUpdater,
+      ),
+    )
+  end inputForInt
+
+  def renderDataList(): Element =
+    ul(
+      children <-- dataSignal.split(_.id) { (id, initial, itemSignal) =>
+        li(child.text <-- itemSignal.map(item => s"${item.count} ${item.label}"))
+      }
+    )
+  end renderDataList
 end Main
